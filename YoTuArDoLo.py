2f482f1acbf5ff2eb8e5f57fb4bfa4c3a8b3a0ba
Nikita Gaidakov <ngaidakov@podfather.com>
2018-05-22 08:47:32 +0100

    Added authentication prompt to class and a main clause

--- YoTuArDoLo.py
+++ YoTuArDoLo.py
@@ -1,16 +1,32 @@
 import tweepy
 import pickle
 
-# so now I just need to use Tweepy
-tw = tweepy.api()
-YoTuArt_handle = 'youtubeartifact'
-YoTuArt = tw.user_timeline(YoTuArt_handle)
-
 
 class YoTuArt:
 
     last_accessed_date = 0
+    auth = None
+    tw = None
+    acc_handles = ['youtubeartifact']
 
     def __init__(self):
-        pass
+        if not self.auth:
+            consumer_key = input('Consumer key: ')
+            consumer_secret = input('Consumer secret: ')
+            access_token = input('Access token: ')
+            access_token_secret = input('Access token secret: ')
 
+            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+            self.auth = auth
+
+            self.tw = tweepy.API(auth)
+
+    def scrape(self):
+        for handle in self.acc_handles:
+            acc = self.tw.user_timeline(handle)
+
+
+if __name__ == 'main':
+    yta_file = 'finish me'
+    y_t_a = YoTuArt()
